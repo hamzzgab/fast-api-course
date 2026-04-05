b44b0eaf27dee7ad61e9bbc74c4be6a927d24651
Hamza Gabajiwala <hamzajg16@gmail.com>
2026-04-05 19:25:10 +0100

    cors

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import lifespan
 from app.routers import posts, users, auth, vote
 
 app = FastAPI(lifespan=lifespan)
 
+origins = ["https://www.google.com", "http://localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def root():
     return {"message": "welcome to the api"}
+
 
 app.include_router(posts.router)
 app.include_router(users.router)
